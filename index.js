0b3017bb4f6d19d27661f3e0d48a27d6ede13fd2
elberto suarez <elbertoisaac@gmail.com>
2024-04-27 08:16:54 -0400

    agregando variables de entorno y configurando puerto

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const http = require("http")
 
 function requestController(){
@@ -6,7 +8,11 @@ function requestController(){
 
 }
 
-
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+
+server.listen(4000, function()
+{
+  console.log("Escuchando en el puerto: " + PORT);
+})
