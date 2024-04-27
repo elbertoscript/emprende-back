1e285183721a52def28016c592ddb195df4b5496
elberto suarez <elbertoisaac@gmail.com>
2024-04-27 09:25:28 -0400

    usando el req, res y el flujo de control if

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ require("dotenv").config()
 
 const http = require("http")
 
-function requestController(){
+function requestController(req,res){
   //lógica de la función
-  console.log("Hola Mundo!!!!");
+  const url = req.url; // aca hace referencia a que url esta intendo ingresar el usuario
+  const method = req.method; // aca el metodo que esta usando
+  console.log({url, method});
+  
+if (method === "GET" && url === "/")
+{
+  res.setHeader("Content-type", "text/html; charset=utf-8")
+  res.write("<h1>Página Principal</h1>")
+  res.end()
+  return
+}
 
+if (method === "GET" && url === "/about")
+{
+  res.setHeader("Content-type", "text/html; charset=utf-8")
+  res.write("<h1>Página about</h1>")
+  res.end()
+return
+}
+
+res.setHeader("Content-type", "text/html; charset=utf-8")
+res.write("<h1>Página no encontrada</h1>")
+res.end()
+return
 }
 
 const server = http.createServer(requestController)
