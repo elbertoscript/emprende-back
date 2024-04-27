eaa8b97f568bb3d935d51d7eec27dbf13ba30c9c
elberto suarez <elbertoisaac@gmail.com>
2024-04-27 15:52:04 -0400

    trabajando con express

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const port = process.env.port
+
+
+//servir archivos estaticos
+app.use(express.static('public'))
+
+//configurar RUTAS
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//poner a escuchar la app en un puerto
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`)
+})
