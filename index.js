08f6b5c6125e7fa3815c213a96ae9d19f5851d10
elberto suarez <elbertoisaac@gmail.com>
2024-07-28 11:30:18 -0400

    nuevo inicio

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ require("dotenv").config()
 
 const http = require("http")
 
-function requestController()
+function requestController(req, res)
 {//lógica de la función
-  console.log("Hola Mundo!!");
+  const url = req.url
+  const method = req.method
+  console.log({url, method});
+
+  if(method === "GET" && url === "/")
+  {
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write("<h1>Página Principal </h1>")
+    res.end()
+    return
+  }
+
+  
+  if(method === "GET" && url === "/about")
+    {
+  
+    }
+
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write("<h1>Página no encontrtada </h1>")
 }
 
 //configurar nuestro servidor
 const server = http.createServer(requestController)
 
-const PORT = process.env.port
+const PORT = process.env.PORT
 
 server.listen(PORT, function()
 {
