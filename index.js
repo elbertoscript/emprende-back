44e7f781dcdbb87cfd09ada6263cb9d9373888b3
elberto suarez <elbertoisaac@gmail.com>
2024-08-07 07:01:03 -0400

    nuevos agragados

--- index.js
+++ index.js
@@ -2,51 +2,53 @@ require("dotenv").config()
 const http = require("http")
 const fs = require("fs")
 
-function requestController(req, res)
-{//lógica de la función
+function requestController(req, res) {
   const url = req.url
   const method = req.method
-  console.log({url, method});
 
-  if(method === "GET" && url === "/")
-  {
+  if (method === "GET" && url === "/") {
     res.setHeader("Content-type", "text/html")
-    fs.readFile('./Public/index.html', function(err, file){
-      if(err){
-        console.log("HUBO UN ERROR");
+    fs.readFile("./public/index.html", function (err, file) {
+      if (err) {
+        console.log("HUBO UN ERROR")
       }
       res.write(file)
       res.end()
     })
-    return 
+    return
   }
-  
-  if(method === "GET" && url === "/About"){
-      res.setHeader("Content-type", "text/html")
-      fs.readFile('./Public/About.html', function(err, file){
-        if(err){
-          console.log(err);
-          return
-        }
-        res.write(file)
-        res.end()
-      })
-      return
-    }
-  
+
+  if (method === "GET" && url === "/about") {
     res.setHeader("Content-type", "text/html")
-    res.write("<h1>Página no encontrada 🥲</h1>")
-    res.end()
-    
+    fs.readFile("./public/about.html", function (err, file) {
+      /* Valores que evaluados en un contexto BOOLEANO, arrojan FALSY:
+        a) null
+        b) undefined
+        c) 0
+        d) ""
+        e) false
+        f) NaN
+      */
+      if (err) {
+        console.log(err)
+        return
+      }
+      res.write(file)
+      res.end()
+    })
+    return
+  }
+
+  res.setHeader("Content-type", "text/html; charset=utf-8")
+  res.write("<h1>Página no encontrada 🥲</h1>")
+  res.end()
 }
 
-//configurar nuestro servidor
+// Configurar nuestro servidor
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
 
-server.listen(PORT, function()
-{
+server.listen(PORT, function () {
   console.log("Aplicación corriendo en puerto: " + PORT)
-}
-)
+})
